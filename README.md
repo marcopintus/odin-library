a4e5e22ff6291f49d11915faa5c17b3fdef78342
marcopintus <marco.pintus93@gmail.com>
2022-06-28 22:10:55 +0200

    It is now possible to add a book via a modal and a form. Furthermore, a book can be removed from the library

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # odin-library
+
+Library web page. It is possibile to add a book specifying title, author, number of pages and wheather it has been already read or not.
+
+Decoration images from: https://pixabay.com/illustrations/ornament-curlicue-waves-lines-art-1332805/ and https://pixabay.com/illustrations/ornament-curlicue-waves-lines-art-1332804/
+
+Rubbish bin icon from: https://materialdesignicons.com/
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 function Book(name,author,pages,read){
     this.name = name;
     this.author = author;
@@ -10,14 +8,115 @@ function Book(name,author,pages,read){
     }
 }
 
-const test = new Book("Test","Nobody",243,true)
-console.log(test.info())
-
-
 function addBookToLibrary(book) {
     myLibrary.push(book);    
   }
 
-addBookToLibrary(test)
+function appendNewBook(book){
+    let newBook = document.createElement("div");
+    newBook.setAttribute("id",book.name.toLowerCase().split(" ").join(""))
+    newBook.classList.add("book");
+    let i = 0;
+    for(let prop  in book){
+        let bookProp = document.createElement("p");
+        bookProp.textContent = book[prop];
+        newBook.appendChild(bookProp);
+        i++;
+        if(i > 2) break
+    }
+    let statusP = document.createElement("p")
+    let statusCheck = document.createElement("input")
+    statusCheck.classList.add("check-box");
+    statusCheck.type = "checkbox";
+    
+    if(book.read){
+        statusCheck.checked = true
+    }
+    statusP.appendChild(statusCheck)
+    newBook.appendChild(statusP)
 
-console.log(myLibrary)
+    let deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("b-remove");
+    let delImg = document.createElement("img");
+    delImg.src = "./icons/rubbish.svg";
+    delImg.classList.add("delete-icon")
+    newBook.setAttribute("id",book.name.toLowerCase().split(" ").join(""))
+    deleteBtn.appendChild(delImg)
+    newBook.appendChild(deleteBtn)
+
+    bookTable.appendChild(newBook)
+}
+
+function submitBook(){
+    let wrtnTitle = document.querySelector(".add-title").value;
+    let wrtnAuthor = document.querySelector(".add-author").value;
+    let wrtnPages = document.querySelector(".add-pages").value;
+    let wrtnCheck = document.querySelector(".add-read").checked;
+    let newBk = new Book(wrtnTitle,wrtnAuthor,wrtnPages,wrtnCheck)
+    let check = checkSame(newBk);
+    if (check == false) {
+        addBookToLibrary(newBk)
+        wipeLibrary()
+        writeLibrary()
+        cCModal()
+    }    
+}
+
+function writeLibrary() {
+    for(i =0; i<myLibrary.length; i++){
+        appendNewBook(myLibrary[i])
+    }
+    delBtn = document.querySelectorAll(".b-remove");
+    if (delBtn){
+        delBtn.forEach(button => {
+        button.addEventListener("click",
+            (e) =>{
+                let bookId = document.getElementById(e.composedPath()[2].id);
+                bookId.remove();
+                myLibrary = myLibrary.filter(book => book.name != e.composedPath()[2].id)
+                }
+        )
+        }
+    )
+    } 
+}
+
+function wipeLibrary(){
+    booksContainer.textContent = '';
+}
+
+function cCModal(){
+    document.querySelector(".add-title").value = '';
+    document.querySelector(".add-author").value = '';
+    document.querySelector(".add-pages").value = '';
+    document.querySelector(".add-read").checked = false;
+    modal.classList.remove("showing");
+}
+
+function checkSame(newBook){
+    return myLibrary.some(book => book == newBook)
+}
+
+
+const bookTable = document.querySelector(".book-table");
+let delBtn = document.querySelectorAll(".b-remove")
+let allBooks = document.querySelectorAll(".book");
+const addBtn = document.querySelector(".add-btn");
+const addForm = document.querySelector(".add-form");
+const modal = document.querySelector(".modal");
+const addBookButton = document.querySelector(".submit-book");
+const booksContainer = document.querySelector(".book-table");
+
+let myLibrary = [];
+
+addBookButton.addEventListener("click",submitBook)
+addBtn.addEventListener("click",() => {
+    modal.classList.add("showing")
+})
+modal.addEventListener("click", (e) => {
+        if(e.composedPath()[0].classList[0] == "modal"){
+            modal.classList.toggle("showing")
+        } 
+})
+
+// make it that it checks if the book already exists
